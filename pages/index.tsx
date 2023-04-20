a9ca1f970014166e164ee63f03cdc8732ad97208
AsijitM <asijit00@gmail.com>
2023-04-20 10:55:16 +0530

    navItems

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google';
 import { getSession, signOut } from 'next-auth/react';
 import { NextPageContext } from 'next';
 import useCurrentUser from '@/hooks/useCurrentUser';
+import Navbar from '@/components/Navbar';
 const inter = Inter({ subsets: ['latin'] });
 
 export async function getServerSideProps(context: NextPageContext) {
@@ -22,14 +23,15 @@ export async function getServerSideProps(context: NextPageContext) {
 
 export default function Home() {
   const { data: user } = useCurrentUser();
-  
+
   return (
     <main>
-      <h1 className="text-3xl text-emerald-500">Netflix Clone</h1>
+      {/* <h1 className="text-3xl text-emerald-500">Netflix Clone</h1>
       <p className="text-white">Logged in as : {user?.name}</p>
       <button onClick={() => signOut()} className="w-full h-10 bg-white">
         Logout
-      </button>
+      </button> */}
+      <Navbar />
     </main>
   );
 }
--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -1,6 +1,7 @@
 import useCurrentUser from '@/hooks/useCurrentUser';
 import { NextPageContext } from 'next';
 import { getSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -20,7 +21,9 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 const Profiles = () => {
-  const { data: user } = useCurrentUser();
+  const router = useRouter();
+    const { data: user } = useCurrentUser();
+    
   return (
     <div className="flex items-center justify-center h-full">
       <div className="flex flex-col">
@@ -28,9 +31,10 @@ const Profiles = () => {
           Who is Watching?
         </h1>
         <div className="flex items-center justify-center gap-8 mt-10 ">
-          <div onClick={() => {}}>
+          <div onClick={() => router.push('/')}>
             <div className="flex-row mx-auto group w-44">
               <div className="flex items-center justify-center overflow-hidden border-2 border-transparent rounded-md w-44 h-44 group-hover:cursor-pointer group-hover:border-white">
+                {/* eslint-disable-next-line @next/next/no-img-element */}
                 <img src="/images/default-blue.png" alt="profile" />
               </div>
 
